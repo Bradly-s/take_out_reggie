1289719901928f68be2b49a4c733307e86c3e1e2
Bradly-s <3308688609@qq.com>
2022-08-18 21:02:09 +0800

    套餐

---
+++ src/main/java/com/jack/reggie/dto/SetmealDto.java
@@ -0,0 +1,14 @@
+package com.jack.reggie.dto;
+
+import com.jack.reggie.entity.Setmeal;
+import com.jack.reggie.entity.SetmealDish;
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class SetmealDto extends Setmeal {
+
+    private List<SetmealDish> setmealDishes;//套餐关联的菜品集合
+
+    private String categoryName;//分类名称
+}
